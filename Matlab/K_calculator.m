57e424e8d59eed75f015c301655a9a5640289da8
Huqo7 <36900398+Huqo7@users.noreply.github.com>
2024-04-08 18:37:19 +0200

    K_calculator
    Script for calculating K value based on overshoot and settling time

---
+++ Matlab/K_calculator.m
@@ -0,0 +1,15 @@
+OS = 5;
+Ts = 0.5;
+
+
+A = [0 1 ; 0 -(K_t*K_b)/(J*R_a)];
+B = [0 ; K_t/(R_a*J)];
+C = [1 0];
+D = 0;
+angle_state_sys = ss(A, B, C, D);
+
+zeta = -log(OS/100)/sqrt(pi^2+log(OS/100)^2);
+wd = 8*tan(acos(zeta));
+poles = [-4/Ts+wd*1i, -4/Ts-wd*1i];
+
+K = acker(A,B,poles)
