d5614907f5262e7e405dd35c02ffd27577e372a4
Huqo7 <36900398+Huqo7@users.noreply.github.com>
2024-04-10 22:21:23 +0200

    Update K_calculator.m

--- Matlab/K_calculator.m
+++ Matlab/K_calculator.m
@@ -1,3 +1,5 @@
+%% Important: remember to run first to set up coefficients
+
 K_t = 0.042;        % Torque Coefficient
 K_b = 0.042;        % Back Emf Constant
 R_a = 8.4;          % Resistance
@@ -7,8 +9,6 @@ J_m = 0.6e-6;       % Inertia of attachment module
 J_d = 1.6e-5;       % Inertia of disc attachment
 
 J = J_r+J_m+J_d;    % Total inertia
-
-%% Characteristics
 
 OS = 15;
 Ts = 2;
@@ -23,9 +23,10 @@ D = 0;
 pole = -4/Ts
 First_Order_K = acker(A,B,pole)
 
-A_cl = A-B*K;
+A_cl = A-B*First_Order_K;
+cl_sys = ss(A_cl,B,C,D);
 
-step(ss(A_cl,B,C,D))
+Kr = 1/dcgain(cl_sys)
 
 %% Second order system
 
@@ -37,8 +38,9 @@ D = 0;
 zeta = -log(OS/100)/sqrt(pi^2+log(OS/100)^2);
 wd = (4/Ts)*tan(acos(zeta));
 poles = [-4/Ts+wd*1i, -4/Ts-wd*1i];
-Second_Order_K = acker(A,B,poles);
-
+Second_Order_K = acker(A,B,poles)
+s=tf('s');
+test = (s-poles(1))*(s-poles(2));
 A_cl = A-B*Second_Order_K;
 
 cl_sys = ss(A_cl, B,C,D);
@@ -54,6 +56,11 @@ f2 = figure(2);
 step(ss(A_cl,B_cl,C,D))
 %% Second order with integrator
 
+A = [0 1 ; 0 -(K_t*K_b)/(J*R_a)];
+B = [0 ; K_t/(R_a*J)];
+C = [1 0];
+D = 0;
+
 Ai=[A zeros(2,1);-C 0]
 Bt = [B; 0]
 Br = [zeros(2,1);1]
@@ -62,22 +69,14 @@ Ci = [C 0];
 zeta = -log(OS/100)/sqrt(pi^2+log(OS/100)^2)
 wd = (4/Ts)*tan(acos(zeta));
 
-s=tf('s');
-eye(3);
-
-#Set poles
-poles = [-4/Ts*20, -4/Ts+wd*1i, -4/Ts-wd*1i];
+%Set poles
+poles = [-(4/Ts)*15, -4/Ts+wd*1i, -4/Ts-wd*1i];
 
 K = acker(Ai,Bt,poles)
 Ai_cl = Ai-Bt*K;
-eig(Ai_cl)
 
-T = Ci*inv(s*eye(3)-Ai_cl)*Br+D
-
-state_space = ss(Ai_cl, Br, Ci, 0);
-transfer = tf(state_space);
-f3 = figure(3)
-step(transfer)
+f3 = figure(3);
+step(ss(Ai_cl, Br, Ci, 0))
 
 %% First order with integrator
 
@@ -91,5 +90,9 @@ Bt = [B; 0];
 Br = [zeros(1,1);1];
 Ci = [C 0];
 
-poles = [-4/Ts*2 -4/Ts]
+poles = [-(4/Ts)*10 -4/Ts]
 First_Order_K = acker(Ai,Bt,poles)
+Ai_cl = Ai-Bt*First_Order_K;
+
+f4 = figure(4); 
+step(ss(Ai_cl,Br,Ci,D));
